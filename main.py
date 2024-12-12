d170ecdc85414922878c4a1d81ee80793aa1c94d
tuancapoo <144972944+tuancapoo@users.noreply.github.com>
2024-12-12 18:36:29 +0700

    Added hello.txt file

--- main.py
+++ main.py
@@ -43,16 +43,16 @@ if __name__ == "__main__":
 
     # Uncomment the following code to test your policy
     # # Reset the environment
-    # observation, info = env.reset(seed=42)
-    # print(info)
+    observation, info = env.reset(seed=42)
+    print(info)
 
-    # policy2210xxx = Policy2210xxx(policy_id=1)
-    # for _ in range(200):
-    #     action = policy2210xxx.get_action(observation, info)
-    #     observation, reward, terminated, truncated, info = env.step(action)
-    #     print(info)
+    policy2210xxx = Policy2210xxx(policy_id=1)
+    for _ in range(200):
+        action = policy2210xxx.get_action(observation, info)
+        observation, reward, terminated, truncated, info = env.step(action)
+        print(info)
 
-    #     if terminated or truncated:
-    #         observation, info = env.reset()
+        if terminated or truncated:
+            observation, info = env.reset()
 
 env.close()
